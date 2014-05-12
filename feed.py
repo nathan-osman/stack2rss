1b787cb9a445bbe29985aca3f5d750aaa8ab6988
Nathan Osman <admin@quickmediasolutions.com>
2014-05-11 17:25:01 -0700

    Added simple minidom-based RSS feed generator.

---
+++ feed.py
@@ -0,0 +1,40 @@
+from email import utils
+from time import time
+from xml.dom.minidom import Document
+
+class RSSFeed(object):
+    """Represents an RSS feed."""
+
+    def __init__(self, title, description):
+        """Initialize the feed."""
+        self._document = Document()
+        rss_element = self._document.createElement('rss')
+        rss_element.setAttribute('version', '2.0')
+        self._document.appendChild(rss_element)
+        self._channel = self._document.createElement('channel')
+        rss_element.appendChild(self._channel)
+        self._channel.appendChild(self._create_text_element('title', title))
+        self._channel.appendChild(self._create_text_element('description', description))
+        self._channel.appendChild(self._create_text_element('lastBuildDate', utils.formatdate(time())))
+
+    def _create_text_element(self, type_, text):
+        """Create an element with a text node."""
+        element = self._document.createElement(type_)
+        element.appendChild(self._document.createTextNode(text))
+        return element
+
+    def append_item(self, title, link=None, description=None, pubDate=None):
+        """Append an item to the feed."""
+        item = self._document.createElement('item')
+        item.appendChild(self._create_text_element('title', title))
+        if not link is None:
+            item.appendChild(self._create_text_element('link', link))
+        if not description is None:
+            item.appendChild(self._create_text_element('description', description))
+        if not pubDate is None:
+            item.appendChild(self._create_text_element('pubDate', pubDate))
+        self._channel.appendChild(item)
+
+    def get_xml(self):
+        """Return the XML for the feed."""
+        return self._document.toxml()
